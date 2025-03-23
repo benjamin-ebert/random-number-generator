78d421d38c061c4b7d3b7bdc334d8753f1e658eb
Benjamin Ebert <bebert410@googlemail.com>
2025-03-23 16:06:48 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: rng <min> <max>")
+		os.Exit(1)
+	}
+
+	min, err1 := strconv.Atoi(os.Args[1])
+	max, err2 := strconv.Atoi(os.Args[2])
+
+	if err1 != nil || err2 != nil {
+		fmt.Println("Please provide two valid integers.")
+		os.Exit(1)
+	}
+
+	if min > max {
+		fmt.Println("The first number must be less than or equal to the second number.")
+		os.Exit(1)
+	}
+
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	randomNumber := rand.Intn(max-min+1) + min
+
+	fmt.Printf("%d\n", randomNumber)
+}
